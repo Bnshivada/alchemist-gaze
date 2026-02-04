14af73bceed7d987362f855700340918c1a70a2a
Shivada <mehmeterensariot15@gmail.com>
2026-02-05 00:18:08 +0300

    Update NoFall.java

--- src/main/java/com/warden/client/modules/NoFall.java
+++ src/main/java/com/warden/client/modules/NoFall.java
@@ -4,14 +4,22 @@ import com.warden.client.modules.Mod.Category;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class NoFall extends Mod {
-    public NoFall() { super("NoFall", Category.PLAYER); }
+    public NoFall() { 
+        super("NoFall", Category.PLAYER); 
+    }
 
     @Override
     public void onTick() {
+        // nullCheck, Mod.java'da tanımlı olmalı
         if (nullCheck()) return;
 
+        // Düşme mesafesi 2.5 bloğu geçtiğinde
         if (mc.player.fallDistance > 2.5f) {
-            mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true, false));
+            // 1.21.4 Paket Yapısı: Sadece yere basıp basmadığını (onGround) gönderir.
+            // Bu, sunucuya "şu an güvenli bir şekilde yere bastım" sinyali verir.
+            if (mc.getNetworkHandler() != null) {
+                mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
+            }
         }
     }
 }
