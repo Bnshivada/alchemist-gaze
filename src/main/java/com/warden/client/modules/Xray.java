b068c0948d6f6997748a3b38cbff652498b45ae5
Shivada <mehmeterensariot15@gmail.com>
2026-02-05 00:04:47 +0300

    Update Xray.java

--- src/main/java/com/warden/client/modules/Xray.java
+++ src/main/java/com/warden/client/modules/Xray.java
@@ -1,21 +1,34 @@
 package com.warden.client.modules;
 
-public class Xray extends Mod {
-    public Xray() { super("Xray", Category.RENDER); }
+// 1. Çarpıyı silen sihirli satır:
+import com.warden.client.modules.Mod.Category;
 
+public class Xray extends Mod {
+    public Xray() { 
+        super("Xray", Category.RENDER); 
+    }
+
+    // Bu değişkeni Mixin sınıfımız kontrol edecek
     public static boolean isXrayEnabled = false;
 
     @Override
     public void onEnable() {
         isXrayEnabled = true;
+        // 1.21.4 Gamma ayarı (Karanlık yerleri aydınlatır)
         mc.options.getGamma().setValue(16.0); 
-        if (mc.worldRenderer != null) mc.worldRenderer.reload();
+        
+        if (mc.worldRenderer != null) {
+            mc.worldRenderer.reload(); // Dünyayı tekrar çizdir
+        }
     }
 
     @Override
     public void onDisable() {
         isXrayEnabled = false;
-        mc.options.getGamma().setValue(1.0);
-        if (mc.worldRenderer != null) mc.worldRenderer.reload();
+        mc.options.getGamma().setValue(1.0); // Parlaklığı normale döndür
+        
+        if (mc.worldRenderer != null) {
+            mc.worldRenderer.reload();
+        }
     }
 }
