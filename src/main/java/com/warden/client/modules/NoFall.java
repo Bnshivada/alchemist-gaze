d7f024ff596a5e649f2dfd267b0933e9e3e07f93
Shivada <mehmeterensariot15@gmail.com>
2026-02-05 00:11:22 +0300

    Create NoFall.java

---
+++ src/main/java/com/warden/client/modules/NoFall.java
@@ -0,0 +1,17 @@
+package com.warden.client.modules;
+
+import com.warden.client.modules.Mod.Category;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+
+public class NoFall extends Mod {
+    public NoFall() { super("NoFall", Category.PLAYER); }
+
+    @Override
+    public void onTick() {
+        if (nullCheck()) return;
+
+        if (mc.player.fallDistance > 2.5f) {
+            mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true, false));
+        }
+    }
+}
